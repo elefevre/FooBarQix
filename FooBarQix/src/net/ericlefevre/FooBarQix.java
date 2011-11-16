9a92d5a2fcd43957e55b94ad8ab47c59a866f4a4
Eric Lefevre-Ardant <contact@ericlefevre.net>
2011-11-16 16:27:30 +0100

    Converts the individual numbers

---
+++ FooBarQix/src/net/ericlefevre/FooBarQix.java
@@ -0,0 +1,36 @@
+package net.ericlefevre;
+
+public class FooBarQix {
+	private static final String TEXT_FOR_7 = "Qix";
+	private static final String TEXT_FOR_5 = "Bar";
+	private static final String TEXT_FOR_3 = "Foo";
+	private static final String[] TEXT = new String[] { "", "", "", TEXT_FOR_3,
+			"", TEXT_FOR_5, "", TEXT_FOR_7, "", "" };
+
+	public String toText(int number) {
+		String result = //
+		""//
+				+ addTextForDivisible(number, 3)//
+				+ addTextForDivisible(number, 5)//
+				+ addTextForDivisible(number, 7)//
+				+ addTextForDigits(number);
+
+		return !result.isEmpty() ? result : Integer.toString(number);
+	}
+
+	private String addTextForDivisible(int number, int digit) {
+		return (number % digit == 0) ? TEXT[digit] : "";
+	}
+
+	private String addTextForDigits(int numberToConvert) {
+		String result = "";
+
+		String asString = Integer.toString(numberToConvert);
+		for (int i = 0; i < asString.length(); i++) {
+			int digit = Integer.parseInt("" + asString.charAt(i));
+			result += TEXT[digit];
+		}
+		return result;
+	}
+
+}
---
+++ FooBarQix/test/net/ericlefevre/FooBarQixTest.java
@@ -0,0 +1,78 @@
+package net.ericlefevre;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class FooBarQixTest {
+	@Test
+	public void should_display_the_number_itself_by_default() {
+		assertThat(new FooBarQix().toText(1), is("1"));
+		assertThat(new FooBarQix().toText(2), is("2"));
+	}
+
+	@Test
+	public void should_display_Foo_or_Bar_or_Qix_based_on_divisibility() {
+		assertThat(new FooBarQix().toText(6), is("Foo"));
+		assertThat(new FooBarQix().toText(9), is("Foo"));
+		assertThat(new FooBarQix().toText(10), is("Bar"));
+		assertThat(new FooBarQix().toText(20), is("Bar"));
+		assertThat(new FooBarQix().toText(14), is("Qix"));
+		assertThat(new FooBarQix().toText(28), is("Qix"));
+	}
+
+	@Test
+	public void should_display_Foo_or_Bar_or_Qix_based_on_digits() {
+		assertThat(new FooBarQix().toText(23), is("Foo"));
+		assertThat(new FooBarQix().toText(31), is("Foo"));
+		assertThat(new FooBarQix().toText(52), is("Bar"));
+		assertThat(new FooBarQix().toText(521), is("Bar"));
+		assertThat(new FooBarQix().toText(17), is("Qix"));
+		assertThat(new FooBarQix().toText(71), is("Qix"));
+	}
+
+	@Test
+	public void should_display_combined_text_based_on_digits() {
+		assertThat(new FooBarQix().toText(352), is("FooBar"));
+		assertThat(new FooBarQix().toText(3571), is("FooBarQix"));
+	}
+
+	@Test
+	public void should_display_text_repeatedly_based_on_digits() {
+		assertThat(new FooBarQix().toText(331), is("FooFoo"));
+	}
+
+	@Test
+	public void should_combine_text_for_multiples_and_for_digits() {
+		assertThat(new FooBarQix().toText(15), is("FooBarBar"));
+		assertThat(new FooBarQix().toText(35), is("BarQixFooBar"));
+	}
+
+	@Test
+	public void should_maintain_the_order_of_the_digits() {
+		assertThat(new FooBarQix().toText(352), is("FooBar"));
+		assertThat(new FooBarQix().toText(53), is("BarFoo"));
+	}
+
+	@Test
+	public void should_execute_specifications_examples_correctly() {
+		assertThat(new FooBarQix().toText(13), is("Foo"));
+		assertThat(new FooBarQix().toText(15), is("FooBarBar"));
+		assertThat(new FooBarQix().toText(33), is("FooFooFoo"));
+		assertThat(new FooBarQix().toText(51), is("FooBar"));
+		assertThat(new FooBarQix().toText(53), is("BarFoo"));
+		assertThat(new FooBarQix().toText(21), is("FooQix"));
+	}
+
+	// 1
+	// 2
+	// FooFoo
+	// 4
+	// BarBar
+	// -Foo
+	// QixQix
+	// 8
+	// -Foo
+	// -Bar
+}
