982344cfc5db8bff8f21baee488e16f5c0aae028
eric.lefevre <eric.lefevre@polyspot.com>
2011-11-24 11:01:39 +0100

    Merged into a single class file

--- FooBarQix/src/net/ericlefevre/FooBarQix.java
+++ FooBarQix/src/net/ericlefevre/FooBarQix.java
@@ -1,12 +1,20 @@
 package net.ericlefevre;
 
 public class FooBarQix {
-	private final IndividualFooBarQix individualFooBarQix = new IndividualFooBarQix();
+	private static final String TEXT_FOR_7 = "Qix";
+	private static final String TEXT_FOR_5 = "Bar";
+	private static final String TEXT_FOR_3 = "Foo";
+	private static final String[] TEXT = new String[] { "", "", "", TEXT_FOR_3,
+			"", TEXT_FOR_5, "", TEXT_FOR_7, "", "" };
+
+	public static void main(String[] args) {
+		System.out.println(new FooBarQix().asTextUpTo(100));
+	}
 
 	public String asTextUpTo(int topNumber) {
 		StringBuilder str = new StringBuilder();
 		for (int i = 1; i <= topNumber; i++) {
-			str.append(individualFooBarQix.toText(i));
+			str.append(toText(i));
 			if (i < topNumber) {
 				str.append("\n");
 			}
@@ -14,8 +22,28 @@ public class FooBarQix {
 		return str.toString();
 	}
 
-	public static void main(String[] args) {
-		System.out.println(new FooBarQix().asTextUpTo(100));
+	public String toText(int number) {
+		StringBuffer result = new StringBuffer();
+		addTextForDivisible(number, 3, result);
+		addTextForDivisible(number, 5, result);
+		addTextForDivisible(number, 7, result);
+		addTextForDigits(number, result);
+
+		return result.length() > 0 ? result.toString() : Integer
+				.toString(number);
 	}
 
+	private void addTextForDivisible(int number, int digit, StringBuffer str) {
+		if (number % digit == 0) {
+			str.append(TEXT[digit]);
+		}
+	}
+
+	private void addTextForDigits(int numberToConvert, StringBuffer str) {
+		String asString = Integer.toString(numberToConvert);
+		for (int i = 0; i < asString.length(); i++) {
+			int digit = Integer.parseInt(asString.substring(i, i + 1));
+			str.append(TEXT[digit]);
+		}
+	}
 }
